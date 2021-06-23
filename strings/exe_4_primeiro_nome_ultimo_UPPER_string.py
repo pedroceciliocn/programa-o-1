4a353815eee7a453f8b0e1fe27634cba2864572c
Pedro Neto <pedroceciliocn@gmail.com>
2021-06-23 17:04:01 -0300

    Update exe_4_primeiro_nome_ultimo_UPPER_string.py
    Editado no qpython no celular

--- strings/exe_4_primeiro_nome_ultimo_UPPER_string.py
+++ strings/exe_4_primeiro_nome_ultimo_UPPER_string.py
@@ -17,3 +17,13 @@ slicing, we can do so backwards:
 s.find("e", 20, -5)
 26
 """
+
+nome = input("Dê seu nome completo: ")
+
+i_fim_primeiro = nome.find(" ") #indice do primeiro espaço
+i_inicio_ultimo = nome.rfind(" ") #indice do primeiro espaço comecando da direita
+
+primeiro = nome[0:i_fim_primeiro].upper()
+ultimo = nome[i_inicio_ultimo + 1:].upper()
+meio = nome[i_fim_primeiro + 1:i_inicio_ultimo].lower().title()
+print(f"{primeiro} {meio} {ultimo}")
